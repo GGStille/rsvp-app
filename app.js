cb2c4efd4cc953f1a124c611629a7454d7d1769f
Graysen Stille <Graysen.Stille@gmail.com>
2018-12-13 13:09:42 -0800

    add filter functionality

--- app.js
+++ app.js
@@ -1,6 +1,38 @@
 const form  = document.getElementById('registar')
 const input = form.querySelector('input')
+
+const mainDiv = document.querySelector('.main')
 const ul    = document.getElementById('invitedList');
+
+const div = document.createElement('div');
+const filterLabel = document.createElement('label');
+const filterCheckBox = document.createElement('input');
+
+filterCheckBox.addEventListener('change', (e) => {
+    const isChecked = e.target.checked;
+    const lis = ul.children;
+    if (isChecked) {
+        for (let i = 0; i < lis.length; i++) {
+            let li = lis[i];
+            if (li.className === 'responded') {
+                li.style.display = '';
+            } else {
+                li.style.display = 'none';
+            }
+        }
+    } else {
+        for (let i = 0; i < lis.length; i++) {
+            let li = lis[i];
+            li.style.display = '';
+        }
+    }
+}) 
+
+filterLabel.textContent = "Hide those who haven't responded";
+filterCheckBox.type = 'checkbox';
+div.appendChild(filterLabel);
+div.appendChild(filterCheckBox)
+mainDiv.insertBefore(div, ul)
 
 function createLi(text) {
     const li = document.createElement('li');
